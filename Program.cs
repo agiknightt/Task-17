1d8e100d1051ac248434a7a4da2f4d47fc07106b
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-09-03 19:52:43 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxCountArray = 30;
+            int[] array = new int[maxCountArray];
+            Random rand = new Random();
+            int maxLocal = int.MinValue;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(1, 50);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i != 0 && i != array.Length - 1)
+                {
+                    if(array[i] > array[i + 1] && array[i] > array[i - 1])
+                    {
+                        Console.Write(array[i] + " ");
+                    }
+                }
+                array[i] = rand.Next(1, 50);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
